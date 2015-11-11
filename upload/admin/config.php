849e2e35a1fef6fb3c423b77f7549e895b6b67a6
sagar <sagar@coupondunia.in>
2015-11-11 22:38:27 +0530

    First commit

---
+++ upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/pustakjugad/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost/pustakjugad/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/pustakjugad/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost/pustakjugad/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/Library/WebServer/Documents/pustakjugad/upload/admin/');
+define('DIR_SYSTEM', '/Library/WebServer/Documents/pustakjugad/upload/system/');
+define('DIR_LANGUAGE', '/Library/WebServer/Documents/pustakjugad/upload/admin/language/');
+define('DIR_TEMPLATE', '/Library/WebServer/Documents/pustakjugad/upload/admin/view/template/');
+define('DIR_CONFIG', '/Library/WebServer/Documents/pustakjugad/upload/system/config/');
+define('DIR_IMAGE', '/Library/WebServer/Documents/pustakjugad/upload/image/');
+define('DIR_CACHE', '/Library/WebServer/Documents/pustakjugad/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Library/WebServer/Documents/pustakjugad/upload/system/storage/download/');
+define('DIR_LOGS', '/Library/WebServer/Documents/pustakjugad/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/Library/WebServer/Documents/pustakjugad/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/Library/WebServer/Documents/pustakjugad/upload/system/storage/upload/');
+define('DIR_CATALOG', '/Library/WebServer/Documents/pustakjugad/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'pustakjugad');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/pustakjugad/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/pustakjugad/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/Library/WebServer/Documents/pustakjugad/upload/catalog/');
+define('DIR_SYSTEM', '/Library/WebServer/Documents/pustakjugad/upload/system/');
+define('DIR_LANGUAGE', '/Library/WebServer/Documents/pustakjugad/upload/catalog/language/');
+define('DIR_TEMPLATE', '/Library/WebServer/Documents/pustakjugad/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/Library/WebServer/Documents/pustakjugad/upload/system/config/');
+define('DIR_IMAGE', '/Library/WebServer/Documents/pustakjugad/upload/image/');
+define('DIR_CACHE', '/Library/WebServer/Documents/pustakjugad/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Library/WebServer/Documents/pustakjugad/upload/system/storage/download/');
+define('DIR_LOGS', '/Library/WebServer/Documents/pustakjugad/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/Library/WebServer/Documents/pustakjugad/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/Library/WebServer/Documents/pustakjugad/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'pustakjugad');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
